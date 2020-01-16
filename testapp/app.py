34181a0c8c7f40d205201e6b00d421019b446f2d
Martijn Faassen <faassen@startifact.com>
2020-01-16 13:41:47 +0100

    Make a test that works with the db.

--- testapp/app.py
+++ testapp/app.py
@@ -1,4 +1,5 @@
 import morepath
+from .models import Person
 
 
 class App(morepath.App):
@@ -13,6 +14,16 @@ class Root(object):
 @App.json(model=Root)
 def hello_world(self, request):
     return {"hello": "world!"}
+
+
+@App.path(model=Person, path="/persons/{id}")
+def get_person(id=0):
+    return Person.objects.filter(pk=id).first()
+
+
+@App.json(model=Person)
+def person_get(self, request):
+    return {"first_name": self.first_name, "last_name": self.last_name}
 
 
 app = App()
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,5 +1,15 @@
-def test_attempt(client):
+import pytest
+
+from testapp.models import Person
+
+
+def test_root(client):
     r = client.get("/")
     assert r.json() == {"hello": "world!"}
-    # r = client.get("/flurb")
-    # assert r.json() == {"hello": "world"}
+
+
+@pytest.mark.django_db
+def test_person_get(client):
+    person = Person.objects.create(first_name="Bob", last_name="Bobson")
+    r = client.get(f"/persons/{person.id}")
+    assert r.json() == {"first_name": "Bob", "last_name": "Bobson"}
